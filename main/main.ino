acecf26134e441d029ccab55f73a06e2c1506209
topsup <topsupp@hotmail.com>
2018-01-14 03:39:22 +0000

    Refined 3 tasks, started task 4
    Got the zumo to stop whilst moving forwards. Set everything up with chars instead of strings for quicker input. Set up the ultrasonic sensor so it detects an object and stops. Next step is to start recording inside processing.

--- main/main.ino
+++ main/main.ino
@@ -4,22 +4,32 @@
 #include <ZumoReflectanceSensorArray.h>
 
 #define LED 13
-#define MIDDLE_THRESHOLD 300
+
+//thresholds for reflectance sensors
+#define MIDDLE_THRESHOLD 400
 #define LR_THRESHOLD 1500
 
+//pins for ultra sonic sensor
+#define trigPin 2
+#define echoPin 6
 
+//number of sensors
 #define NUM_SENSORS 6
 
+//motor sensors
 #define TURN_BASE_SPEED  100
-#define TURN_DELAY       1600
-#define REVERSE_SPEED    100
+#define TURN_DELAY       1600      //90 degree turn
+#define FINE_TURN_DELAY  1600 / 9  //10 degree fine turn
+#define REVERSE_SPEED    100       
 #define REVERSE_DURATION 300
 #define FORWARD_SPEED    200
 #define LEFT_SPEED       170
 
 unsigned int sensor_values[NUM_SENSORS];
-unsigned long startTime = millis();
+unsigned long interval = 1000;
+unsigned long previousMillis = 0;
 
+//booleans for rooms, corridors and zumo being automated
 bool automatic = false;
 bool leftRoom = false;
 bool rightRoom = false;
@@ -30,12 +40,16 @@ ZumoMotors motors;
 ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
 Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
 
-//int incomingByte;
+//string for incoming string, sent through port over processing
 String incomingString;
+int incomingChar;
 
 void setup() {
   //initialize serial communications
   Serial.begin(9600);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   
   digitalWrite(13, HIGH);
   button.waitForButton();
@@ -49,68 +63,119 @@ void loop() {
   sensors.read(sensor_values);
  
   if (Serial.available() > 0) {
-    incomingString = Serial.readString();
 
-    if (incomingString == "w" || incomingString == "c") {
 
-      if (incomingString == "c") {
+      incomingChar = Serial.read();
+
+    
+    
+    if (incomingChar == 'w' || incomingChar == 'c') 
+    {
+      if (incomingChar == 'c') 
+      {
         Serial.println("turnC");
         Serial.println("Turn complete.");        
       }
       
       roomOrCorridor();
-      
+
       automatic = true;
+      
       Serial.println("Going forwards.");
       while (automatic == true) //code will always run unless told to stop
       {
-        incomingString = Serial.read(); //reads serial input and places into a String variable
+        incomingChar = Serial.read(); //reads serial input and places into a String variable
+        
+        if (incomingChar == 'x')
+        {
+          automatic = false;
+          motors.setSpeeds(0,0);
+          delay(100);
+          break;
+        }
         
         motors.setSpeeds(LEFT_SPEED, FORWARD_SPEED); //move forward
         moveAndCheckForLines();    //keep Zumo within track borders
       }
     }
-    else if (incomingString == "a") {
-      Serial.println("Turning left.");
+    else if (incomingChar == 'a') {
+      Serial.println("leftTurn");
+      Serial.println("Turning left 90 degrees.");
       motors.setSpeeds(-TURN_BASE_SPEED, TURN_BASE_SPEED); //move left
       delay(TURN_DELAY);
       motors.setSpeeds(0,0);
     }
-    else if (incomingString == "s") {
+    else if (incomingChar == 'A') {
+      Serial.println("Turning left 10 degrees.");
+      motors.setSpeeds(-TURN_BASE_SPEED, TURN_BASE_SPEED); //move left
+      delay(FINE_TURN_DELAY);
+      motors.setSpeeds(0,0);
+    }
+    else if (incomingChar == 's') {
       Serial.println("Reversing.");
       motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED); //move backwards
     }
-    else if (incomingString == "d") {
-      Serial.println("Turning right.");
+    else if (incomingChar == 'd') {
+      Serial.println("rightTurn");
+      Serial.println("Turning right 90 degrees.");
       motors.setSpeeds(TURN_BASE_SPEED, -TURN_BASE_SPEED); //move left
       delay(TURN_DELAY);
       motors.setSpeeds(0,0);
     }
-    else if (incomingString == "x") {
+    else if (incomingChar == 'D') {
+      Serial.println("Turning right 10 degrees.");
+      motors.setSpeeds(TURN_BASE_SPEED, -TURN_BASE_SPEED); //move left
+      delay(FINE_TURN_DELAY);
+      motors.setSpeeds(0,0);
+    }
+    else if (incomingChar == 'x') {
       Serial.println("Stopping.");
+      automatic = false;
       motors.setSpeeds(0, 0); //stop
     }
-    else if (incomingString == "rRo")
+    else if (incomingChar == 'r')
     {
-      Serial.println("rRo");
-      Serial.println("Room to the right.");
-      rightRoom = true;
-      
+      incomingChar = Serial.read();
+      if (incomingChar == 'R')
+      {
+        Serial.println("rRo");
+        Serial.println("Room to the right.");
+        rightRoom = true;
+      }
+      if (incomingChar == 'C')
+      {
+        Serial.println("rCo");
+        Serial.println("Corridor to the right.");
+        rightCorridor = true;
+      }
     }
-    else if (incomingString == "lRo")
-    {      
+    else if (incomingChar == 'l')
+    {
+      incomingChar = Serial.read();
+      if (incomingChar == 'R')
+      {
+      Serial.println("lRo");   
       Serial.println("Room to the left.");
       leftRoom = true;
+      }
+      if (incomingChar == 'C')
+      {
+        Serial.println("lCo");
+        Serial.println("Corridor to the left.");
+        leftCorridor = true;
+      }
     }
-    else if (incomingString == "rCo")
+    else if (incomingChar == 'R')
     {
-      Serial.println("Corridor to the right.");
-      rightCorridor = true;
-    }
-    else if (incomingString == "lCo")
-    {
-      Serial.println("Corridor to the left.");
-      leftCorridor = true;
+      incomingChar = Serial.read();
+      if (incomingChar == 'E')
+      {
+        enterRoom();
+      }
+      else if (incomingChar == 'L')
+      {
+        exitRoom();
+      }
     }
   }
 }
@@ -132,23 +197,11 @@ if ((sensor_values[1] > MIDDLE_THRESHOLD) || (sensor_values[2] > MIDDLE_THRESHOL
 
 else if (sensor_values[0] > LR_THRESHOLD)
 {   
-//  if (wallHit == 0)
-//  {
-//    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-//    delay(REVERSE_DURATION);
-//    automatic = false;
-//    motors.setSpeeds(0,0);
-//    Serial.println("L Wall detected, stopping.");
-//  }
-//  else
-//  {
     motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
     delay(REVERSE_DURATION);
-    motors.setSpeeds(TURN_BASE_SPEED + 30, -TURN_BASE_SPEED);
+    motors.setSpeeds(TURN_BASE_SPEED, -TURN_BASE_SPEED);
     delay(100);
     motors.setSpeeds(LEFT_SPEED, FORWARD_SPEED);
-//    wallHit = 1;
-//  }
 }
 else if (sensor_values[5] > LR_THRESHOLD)
 {
@@ -157,23 +210,12 @@ else if (sensor_values[5] > LR_THRESHOLD)
     motors.setSpeeds(-TURN_BASE_SPEED, TURN_BASE_SPEED);
     delay(100);
     motors.setSpeeds(LEFT_SPEED, FORWARD_SPEED);
-//    wallHit = 0;
-//  }
 }
 else 
 {
   motors.setSpeeds(LEFT_SPEED, FORWARD_SPEED);
 }
 }
-//  if (isAtDeadEnd())
-//  {
-//    Serial.println("F Wall detected, stopping.");
-//    motors.setSpeeds(-SPEED, -SPEED); //reverse
-//    delay(200); //for 200ms
-//    motors.setSpeeds(0, 0);
-//    delay(1000); //for 200ms
-//    incomingString = "x";
-//  }
 
 
 bool overLine(int sensorPin) {
@@ -183,18 +225,72 @@ bool overLine(int sensorPin) {
 bool roomOrCorridor() {
   if (leftRoom == true) {
     Serial.print("Entering left room.\n");
-    leftRoom == false;
   }
   else if (rightRoom == true) {
     Serial.print("Entering right room.\n");
-    rightRoom == false;
   }
   else if (leftCorridor == true) {
     Serial.print("Entering right corridor.\n");
-    leftCorridor == false;
   }
   else if (rightCorridor == true) {
     Serial.print("Entering right room.\n");
-    rightCorridor == false;
   }
 }
+
+void enterRoom() {
+  
+  motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  delay(200);
+  motors.setSpeeds(0,0);  
+  delay(100);
+  motors.setSpeeds(-TURN_BASE_SPEED, TURN_BASE_SPEED);
+  delay(TURN_DELAY);
+  
+  for (int x = 0; x < 9; x++) {
+    motors.setSpeeds(TURN_BASE_SPEED, -TURN_BASE_SPEED);
+    delay(TURN_DELAY * 4 / 9);
+    long duration, distance;
+    digitalWrite(trigPin, LOW);  // Added this line
+    delayMicroseconds(2); // Added this line
+    digitalWrite(trigPin, HIGH);
+  //  delayMicroseconds(1000); - Removed this line
+    delayMicroseconds(10); // Added this line
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2) / 29.1;
+  
+    if (distance >= 18 || distance <= 0){
+      Serial.println("Not found.");
+    }
+    else {
+      Serial.println("Object found.");
+      motors.setSpeeds(0,0);
+    }
+  }
+  motors.setSpeeds(0,0);
+  
+}
+void exitRoom() {
+
+  if (leftRoom)
+  {
+    motors.setSpeeds(-100, -100);
+    delay(200);
+    Serial.print("Left left room.\n");
+    motors.setSpeeds(TURN_BASE_SPEED, -TURN_BASE_SPEED); //move left
+    delay(TURN_DELAY);
+    motors.setSpeeds(0,0);
+    leftRoom = false;
+  }
+  else if (rightRoom)
+  {
+    motors.setSpeeds(-100, -100);
+    delay(200);
+    Serial.print("Left right room.\n");
+    motors.setSpeeds(-TURN_BASE_SPEED, TURN_BASE_SPEED); //move left
+    delay(TURN_DELAY);
+    motors.setSpeeds(0,0);
+    rightRoom = false;
+  }
+}
+
